83be99519e9a399e62acf75c6b7ef054eac32773
Chamath Viduranga <chamath.viduranga@airudi.com>
2023-05-10 23:03:09 +0530

    add _error page

--- src/frontend/medi-help/pages/_app.tsx
+++ src/frontend/medi-help/pages/_app.tsx
@@ -1,7 +1,8 @@
 import "../styles/globals.css";
 import { MyAppProps } from "@/components/common/types";
 import { Layouts } from "@/components/common/layouts";
-function MyApp({ Component, pageProps }: MyAppProps) {
+
+function MyApp({ Component, pageProps}: MyAppProps) {
   const Layout = Layouts[Component.Layout] ?? ((page: any) => page);
   return (
     <Layout>
